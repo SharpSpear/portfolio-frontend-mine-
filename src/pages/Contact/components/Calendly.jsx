bc453fb09d18d658716d05d4d1ce75081b0ec269
SharpSpear <spearbig1021@gmail.com>
2022-06-24 10:34:40 +0900

    first

---
+++ src/pages/Contact/components/Calendly.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { InlineWidget } from 'react-calendly';
+
+const Calendly = () => {
+  return (
+    <div>
+      <InlineWidget url="https://calendly.com/spearbig1021" />
+    </div>
+  );
+};
+
+export default Calendly;
